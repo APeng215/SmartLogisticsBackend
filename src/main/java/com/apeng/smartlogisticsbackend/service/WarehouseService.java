2c84b431054de46ca5e667d3fd9bc9cef875700e
A_Peng215 <976310604@qq.com>
2024-06-28 15:22:12 +0800

    实现仓库Service

--- src/main/java/com/apeng/smartlogisticsbackend/service/WarehouseService.java
+++ src/main/java/com/apeng/smartlogisticsbackend/service/WarehouseService.java
@@ -5,11 +5,9 @@ import com.apeng.smartlogisticsbackend.entity.Warehouse;
 import java.util.List;
 
 public interface WarehouseService {
-
     Long insert(Warehouse warehouse);
     void deleteById(Long id);
     Warehouse findById(Long id);
     List<Warehouse> findAll();
     Warehouse update(Warehouse warehouse);
-
 }
