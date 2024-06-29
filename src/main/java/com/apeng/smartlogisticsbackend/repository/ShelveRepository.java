39ff1984e775d066f1d03868c1016ddbd6ee679c
pipixiya <119400330+pipixiya@users.noreply.github.com>
2024-06-29 16:59:26 +0800

    订单存放到货架的算法编写 (#7)

--- src/main/java/com/apeng/smartlogisticsbackend/repository/ShelveRepository.java
+++ src/main/java/com/apeng/smartlogisticsbackend/repository/ShelveRepository.java
@@ -4,5 +4,8 @@ import com.apeng.smartlogisticsbackend.entity.Shelve;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ShelveRepository extends JpaRepository<Shelve, Long> {
+    List<Shelve> findShelvesByWarehouseId(Long id);
 }
