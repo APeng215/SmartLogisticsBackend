e3fd4c8ca4f1bf42be5361adf7df5808c26376d5
A_Peng215 <976310604@qq.com>
2024-07-02 16:58:53 +0800

    优化代码

--- src/main/java/com/apeng/smartlogisticsbackend/controller/RegisterController.java
+++ src/main/java/com/apeng/smartlogisticsbackend/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.apeng.smartlogisticsbackend.controller;
 
 import com.apeng.smartlogisticsbackend.dto.RegisterRequest;
+import com.apeng.smartlogisticsbackend.entity.User;
 import com.apeng.smartlogisticsbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,8 @@ public class RegisterController {
     private UserService userService;
 
     @PostMapping
-    public void register(@RequestBody RegisterRequest registerRequest) {
-        userService.register(registerRequest);
+    public User register(@RequestBody RegisterRequest registerRequest) {
+        return userService.register(registerRequest);
     }
 
 }
