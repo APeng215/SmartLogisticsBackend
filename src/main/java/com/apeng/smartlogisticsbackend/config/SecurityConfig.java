36364cf3741ef182ef90f41805d228f0365a21e3
A_Peng215 <976310604@qq.com>
2024-07-02 16:06:34 +0800

    完成登录安全框架

--- src/main/java/com/apeng/smartlogisticsbackend/config/SecurityConfig.java
+++ src/main/java/com/apeng/smartlogisticsbackend/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.apeng.smartlogisticsbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.RememberMeAuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,13 +15,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.security.SecureRandom;
+import java.util.Base64;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig implements WebMvcConfigurer {
+
+    private static final String KEY = generateRandomKey();
 
     /**
      * 允许跨域请求
@@ -33,11 +46,34 @@ public class SecurityConfig implements WebMvcConfigurer {
     }
 
     @Bean
+    RememberMeAuthenticationFilter rememberMeFilter(AuthenticationManager authenticationManager, RememberMeServices rememberMeServices) {
+        return new RememberMeAuthenticationFilter(authenticationManager, rememberMeServices);
+    }
+
+    @Bean
+    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+        return new TokenBasedRememberMeServices(KEY, userDetailsService);
+    }
+
+    @Bean
+    RememberMeAuthenticationProvider rememberMeAuthenticationProvider() {
+        return new RememberMeAuthenticationProvider(KEY);
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
     }
 
     /**
@@ -58,19 +94,33 @@ public class SecurityConfig implements WebMvcConfigurer {
      * @throws Exception
      */
     @Bean
-    public SecurityFilterChain configureSecurityChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configureSecurityChain(HttpSecurity http, RememberMeAuthenticationFilter rememberMeAuthenticationFilter, DaoAuthenticationProvider daoAuthenticationProvider) throws Exception {
         http
+                .exceptionHandling(handing -> handing.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .authenticationProvider(daoAuthenticationProvider)
+                .authenticationProvider(rememberMeAuthenticationProvider())
+                .formLogin(config -> config
+                        .successHandler((req, res, auth) -> res.setStatus(HttpStatus.NO_CONTENT.value()))
+                        .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                )
+                .addFilterAfter(rememberMeAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeHttpRequests -> {
                     authorizeHttpRequests
-                            .requestMatchers("/swagger-ui/**").permitAll()
                             .requestMatchers("/v3/**").permitAll()
                             .requestMatchers("/register").permitAll()
                             .requestMatchers("/error").permitAll()
                             .requestMatchers("/**").hasRole("USER");
                 })
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults());
+                .logout(config -> config.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)))
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    private static String generateRandomKey() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] keyBytes = new byte[32]; // 32 bytes for a 256-bit key
+        secureRandom.nextBytes(keyBytes);
+        return Base64.getEncoder().encodeToString(keyBytes);
     }
 
 }
