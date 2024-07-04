3ca67014488e22ec0615c609e4aeceeebd91d812
Laojia <3228628142@qq.com>
2024-07-04 17:55:41 +0800

    添加车俩的容量，添加autoInAndOutbound（自动化出入库）--todo

--- src/main/java/com/apeng/smartlogisticsbackend/service/WarehouseService.java
+++ src/main/java/com/apeng/smartlogisticsbackend/service/WarehouseService.java
@@ -18,5 +18,5 @@ public interface WarehouseService {
     Warehouse update(Warehouse warehouse);
     boolean inbound(InboundRequest inboundRequest);
     List<Long> outbound(OutboundRequest outboundRequest);
-    boolean autoIObound(long carId, List<Warehouse> passWarehouses);
+    boolean autoInAndOutbound(long carId, List<Warehouse> passWarehouses);
 }
